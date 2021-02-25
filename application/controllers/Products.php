2a84ad4386b5a2cef9df40730a7c19dc1c42ff3c
Sanket1011 <41885812+Sanket1011@users.noreply.github.com>
2021-02-25 09:07:01 +0530

    Update Products.php

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -6,29 +6,29 @@ class Products extends CI_Controller{
     function  __construct(){
         parent::__construct();
         
-        // Load cart library
+      
         $this->load->library('cart');
         
-        // Load product model
+        
         $this->load->model('product');
     }
     
     function index(){
         $data = array();
         
-        // Fetch products from the database
+      
         $data['products'] = $this->product->getRows();
         
-        // Load the product list view
+       
         $this->load->view('products/index', $data);
     }
     
     function addToCart($proID){
         
-        // Fetch specific product by ID
+        
         $product = $this->product->getRows($proID);
         
-        // Add product to the cart
+        
         $data = array(
             'id'    => $product['id'],
             'qty'    => 1,
@@ -38,7 +38,7 @@ class Products extends CI_Controller{
         );
         $this->cart->insert($data);
         
-        // Redirect to the cart page
+        
         redirect('cart/');
     }
     
