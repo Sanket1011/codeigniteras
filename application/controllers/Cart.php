3d767b6b3f089c8cf0e57c107dc673069d863b83
Sanket1011 <41885812+Sanket1011@users.noreply.github.com>
2021-02-25 09:06:21 +0530

    Update Cart.php

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -6,31 +6,31 @@ class Cart extends CI_Controller{
     function  __construct(){
         parent::__construct();
         
-        // Load cart library
+        
         $this->load->library('cart');
         
-        // Load product model
+        
         $this->load->model('product');
     }
     
     function index(){
         $data = array();
         
-        // Retrieve cart data from the session
+        
         $data['cartItems'] = $this->cart->contents();
         
-        // Load the cart view
+        
         $this->load->view('cart/index', $data);
     }
     
     function updateItemQty(){
         $update = 0;
         
-        // Get cart item info
+        
         $rowid = $this->input->get('rowid');
         $qty = $this->input->get('qty');
         
-        // Update item in the cart
+      t
         if(!empty($rowid) && !empty($qty)){
             $data = array(
                 'rowid' => $rowid,
@@ -39,15 +39,15 @@ class Cart extends CI_Controller{
             $update = $this->cart->update($data);
         }
         
-        // Return response
+       
         echo $update?'ok':'err';
     }
     
     function removeItem($rowid){
-        // Remove item from cart
+        
         $remove = $this->cart->remove($rowid);
         
-        // Redirect to the cart page
+       
         redirect('cart/');
     }
     
