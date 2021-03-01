65bf35689d76febe110228492d9838ea1226e0f5
Sanket1011 <41885812+Sanket1011@users.noreply.github.com>
2021-03-01 14:12:39 +0530

    Add files via upload

---
+++ application/views/includes/header.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<script
+
+src="https://ajax.googleapis.com/ajax/libs/jquery /3.2.1/jquery.min.js"></script>
+
+<script
+
+src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Basic Crud operation in Codeigniter 3</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+</head>
